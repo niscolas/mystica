8e4a4200174f0dd378ad16609de719026cbe94fe
niscolas <niscolas@proton.me>
2025-01-22 18:33:50 -0300

    misc: updates PawnUIWidget to PlayerUIWidget

--- Source/Mystica/PlayerUIWidget.h
+++ Source/Mystica/PlayerUIWidget.h
@@ -3,20 +3,20 @@
 #include "CoreMinimal.h"
 #include "Mystica/PlayerUIComponent.h"
 #include "UObject/Interface.h"
-#include "PawnUIWidget.generated.h"
+#include "PlayerUIWidget.generated.h"
 
-class IPawnUIComponent;
+class UPlayerUIComponent;
 
 UINTERFACE(MinimalAPI)
 
-class UPawnUIWidget : public UInterface {
+class UPlayerUIWidget : public UInterface {
     GENERATED_BODY()
 };
 
-class MYSTICA_API IPawnUIWidget {
+class MYSTICA_API IPlayerUIWidget {
     GENERATED_BODY()
 
 public:
     UFUNCTION(BlueprintNativeEvent, BlueprintCallable)
-    void PawnUIWidgetInitialize(UPlayerUIComponent *OwningPlayerUIComponent);
+    void PlayerUIWidgetInitialize(UPlayerUIComponent *OwningPlayerUIComponent);
 };
